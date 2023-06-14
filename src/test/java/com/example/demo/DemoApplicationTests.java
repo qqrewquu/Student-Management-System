06850c60c43bfaeeb6f319417e8db2570fb1c77a
Gary Guo <15zg17@queensu.ca>
2023-06-14 10:42:37 -0400

    practice unit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,12 +4,29 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
 	@Test
-	void contextLoads() {
-//		Assertions.fail("Oops, test failed");
+	void itShouldAddTwoNumbers() {
+		// given
+		int numOne = 20;
+		int numtwo = 30;
+
+		// when
+		int result = underTest.add(numOne, numtwo);
+
+		// then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator{
+		public int add(int a, int b){
+			return a + b;
+		}
 	}
 
 }
